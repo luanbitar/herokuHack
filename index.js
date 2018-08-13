c6cf281b03610a4a32194c7472020e3b4a7221ed
luanbitar <bitarluan@gmail.com>
2018-08-13 18:57:05 -0400

    Making log better

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-const fetch = require('node-fetch')
+const fetch = require('node-fetch');
+const { send } = require('micro');
 
-module.exports = async () => {
+module.exports = (req, res) => {
   setInterval(async () => {
     const request = await fetch('https://fpf-tracking.herokuapp.com/api/tracking')
     const data = await request.json();
-    return data;
+    console.log(data);
+    console.log(`\n\n\n\n\n\n\n\n\n\n new Request on: ${new Date()}`);
   }, 300000);
 }
